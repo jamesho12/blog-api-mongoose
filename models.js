ab589f168f8266dc1d0dd2f3472bbae25534f4bf
chaoseternai <jamesho.AHS@gmail.com>
2018-07-18 17:36:40 -0700

    Fixed collection lower case

--- models.js
+++ models.js
@@ -28,7 +28,7 @@ blogPostsSchema.serialize = function() {
   };
 }
 
-const BlogPosts = mongoose.model('BlogPosts', blogPostsSchema);
+const BlogPosts = mongoose.model('Blogposts', blogPostsSchema);
 
 // what is the proper naming convention for collections? Camel case or lowercase?
 // so in the example mongoose.model breaks it down from 'Restaurants' to 'restaurants', how would this work for 'BlogPosts'?
