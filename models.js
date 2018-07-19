b04a6a5a151f58220233c2d9799b0d0b7df91192
chaoseternai <jamesho.AHS@gmail.com>
2018-07-18 17:33:29 -0700

    implemented get

---
+++ models.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+
+const blogPostsSchema = mongoose.Schema({
+  title: { type: String, required: true },
+  content: { type: String, required: true },
+  author: {
+    firstName: { type: String, required: true},
+    lastName: { type: String, required: true}
+  },
+  created: { type: Date, default: Date.now}
+});
+
+// so this virutal and serialize is needed to put together firstName and lastName for author?
+
+blogPostsSchema.virtual('authorString').get(function() {
+  return `${this.author.firstName} ${this.author.lastName}`.trim();
+});
+
+// even without a virtual, we would still need to serialize correct?
+
+blogPostsSchema.serialize = function() {
+  return {
+    id: this._id,
+    title: this.title,
+    content: this.content,
+    author: this.authorString,
+    create: this.created
+  };
+}
+
+const BlogPosts = mongoose.model('BlogPosts', blogPostsSchema);
+
+// what is the proper naming convention for collections? Camel case or lowercase?
+// so in the example mongoose.model breaks it down from 'Restaurants' to 'restaurants', how would this work for 'BlogPosts'?
+// will it be 'blogPosts' or 'blogposts'
---
+++ server.js
@@ -0,0 +1,69 @@
+'use strict';
+
+const express = require('express');
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+
+const { PORT, DATABASE_URL } = require('./config');
+const { BlogPosts } = require('./models');
+
+const app = express();
+app.use(express.json());
+
+app.get('/blogPosts', (req, res) => {
+  BlogPosts
+    .find()
+    .then(posts => {
+      res.json({
+        blogs: blogs.map(blog => blog.serialize())
+      });
+    })
+    .catch(
+      err => {
+        console.error(err);
+        res.status(500).json({ message: 'Internal server error' });
+      }
+    )
+});
+
+let server;
+
+function runServer(databaseUrl, port=PORT) {
+  return new Promise((resolve, reject) => {
+    mongoose.connect(databaseUrl, err => {
+      if(err) {
+        return reject(err);
+      }
+
+      server = app.listen(port, () => {
+        console.log(`Your app is listening on port ${port}`);
+        resolve();
+      })
+      .on('error', err => {
+        mongoose.disconnect();
+        reject(err);
+      });
+    });
+  });
+}
+
+function closeServer() {
+  return mongoose.disconnect().then(() => {
+    return new Promise((resolve, reject) => {
+      console.log('Closing server');
+      server.close(err => {
+        if(err) {
+          return reject(err);
+        }
+        resolve();
+      });
+    });
+  });
+}
+
+if(require.main === module) {
+  runServer(DATABASE_URL).catch(err => console.error(err));
+}
+
+module.exports = { app, runServer, closeServer };
